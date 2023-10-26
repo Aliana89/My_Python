e46a2d1dea0abb5a4384664a25b5e8798ec4e9b4
Aliana89 <alyanaay@mail.ru>
2023-10-26 20:28:54 +0300

    Seminar_Python\Task_4.py

--- Seminar_Python/Task_4.py
+++ Seminar_Python/Task_4.py
@@ -2,10 +2,18 @@
 оно является, то есть выведите такое число n, что φ(n)=A. Если А не является
 числом Фибоначчи, выведите число -1.
 Input: 5 Output: 6"""
-n = 
-a = int(input("Введите натуральное  число а>1: "))
-
-while 
-fib(n) == a
-print -1
-if n == 1
+n0 = 0 # n-2 элемент
+n1 = 1 # n-1 элемент
+n = 1 # n = (n-2)+(n-1)
+count = 1
+res = 0
+while (res<n):
+    res = n0+n1
+n0 = n1
+n1 = res
+count += 1
+#print(res)
+if res==n:
+    print(count)
+else:
+    print(-1)
