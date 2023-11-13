348cc209786706b659ec174ab5cc315fae71d10a
Aliana89 <alyanaay@mail.ru>
2023-11-13 14:05:08 +0300

    H0me_Work_5\Task_2.py

---
+++ H0me_Work_5/Task_2.py
@@ -0,0 +1,16 @@
+"""Задача 32: Определить индексы элементов массива (списка),
+значения которых принадлежат заданному диапазону (т.е. не
+меньше заданного минимума и не больше заданного
+максимума)
+"""
+list_1 = [-5, 9, 0, 3, -1, -2, 1, 4, -2, 10, 2, 0, -9, 8, 10, -9, 0, -5, -5, 7]
+min_number = 5
+max_number = 15
+
+result_indices = []
+for i in range(len(list_1)):
+    if min_number <= list_1[i] <= max_number:
+        result_indices.append(i)
+
+for index in result_indices:
+    print(index)
