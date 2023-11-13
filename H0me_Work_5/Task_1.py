0f2240a2713d50e40bca107a43b333f43a845b53
Aliana89 <alyanaay@mail.ru>
2023-11-13 14:04:47 +0300

    H0me_Work_5\Task_1.py

---
+++ H0me_Work_5/Task_1.py
@@ -0,0 +1,24 @@
+
+"""a1 = 2
+d = 3
+n = 4
+
+progression = []
+
+for i in range(n):
+    progression.append(a1 + i*d)
+
+print(*progression)"""
+
+
+a1 = 2
+d = 3
+n = 4
+
+progression = []
+
+for i in range(n):
+    progression.append(a1 + i*d)
+
+for num in progression:
+    print(num)
