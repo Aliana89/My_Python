4aa2dc39e8242ecd4fd1906c8ea5aed07a0ccf18
Aliana89 <alyanaay@mail.ru>
2023-11-13 19:05:00 +0300

    Seminar_Python\Task_45.py

--- Seminar_Python/Task_45.py
+++ Seminar_Python/Task_45.py
@@ -15,21 +15,21 @@
 пару не дает).
 Ввод: Вывод:
 300 220 284"""""
-def get_divisors_sum(num):
-    # Функция для получения суммы делителей числа, исключая само число
-    divisors_sum = 0
-    for i in range(1, num):
-        if num % i == 0:
-            divisors_sum += i
-    return divisors_sum
+def sum_divisors(n):
+    sum_div = 0
+    for i in range(1, n):
+        if n % i == 0:
+            sum_div += i
+    return sum_div
 
 k = int(input("Введите число k: "))
 
-friendly_pairs = []
-for n in range(1, k):
-    m = get_divisors_sum(n)
-    if get_divisors_sum(m) == n and n < m:
-        friendly_pairs.append((n, m))
+result_list = []
 
-for pair in friendly_pairs:
-    print(pair[0], pair[1])
+for n in range(1, k+1):
+    m = sum_divisors(n)
+    if sum_divisors(m) == n and m != n:
+        result_list.append((n, m))
+
+for result in result_list:
+    print(result, end=" ")
