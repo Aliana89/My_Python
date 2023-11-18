22596d3670b8dbdf5712fa1e4e468efb58b61a02
Aliana89 <alyanaay@mail.ru>
2023-11-18 07:23:35 +0300

    Seminar_Python\Task_45.py

--- Seminar_Python/Task_45.py
+++ Seminar_Python/Task_45.py
@@ -14,7 +14,7 @@
 выведена только один раз (перестановка чисел новую
 пару не дает).
 Ввод: Вывод:
-300 220 284"""""
+300 220 284
 def sum_divisors(n):
     sum_div = 0
     for i in range(1, n):
@@ -32,4 +32,22 @@ for n in range(1, k+1):
         result_list.append((n, m))
 
 for result in result_list:
-    print(result, end=" ")
+    print(result)"""
+def sum_divisors(num):
+    # Функция для получения суммы делителей числа, исключая само число
+    divisors_sum = 0
+    for i in range(1, num):
+        if num % i == 0:
+            divisors_sum += i
+    return divisors_sum
+
+k = int(input("Введите число k: "))
+
+friendly_pairs = []
+for n in range(1, k):
+    m = sum_divisors(n)
+    if sum_divisors(m) == n and n < m:
+        friendly_pairs.append((n, m))
+
+for pair in friendly_pairs:
+    print(pair)
